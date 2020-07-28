0aff5062995c5dc4a8b4867a26a3f93871549727
Alex Matseshe <amatseshe@gmail.com>
2020-07-27 23:14:50 -0500

    Remove material-ui and add Chakra-ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,31 @@
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import ProTip from '../src/ProTip';
-import Link from '../src/Link';
-import Copyright from '../src/Copyright';
+import Link from 'next/link';
+import {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  Button,
+} from '@chakra-ui/core';
+import Layout from '../components/Layout';
 
-export default function Index() {
-  return (
-    <Container maxWidth="sm">
-      <Box my={4}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Next.js with TypeScript example
-        </Typography>
-        <Link href="/about" color="secondary">
-          Go to the about page
-        </Link>
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
-  );
-}
+const IndexPage = () => (
+  <Layout title="Home | Next.js + TypeScript Example">
+    <h1>Hello Next.js 👋</h1>
+    <p>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+      <Accordion allowToggle>
+        <AccordionItem>
+          <AccordionButton>Accordion 1</AccordionButton>
+          <AccordionPanel>Welcome home</AccordionPanel>
+        </AccordionItem>
+      </Accordion>
+      <Button colorScheme="red" marginTop="40px">
+        Welcome
+      </Button>
+    </p>
+  </Layout>
+);
+
+export default IndexPage;
